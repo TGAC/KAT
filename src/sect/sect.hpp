1d90ab387e48752255ad5b34f3c4fa9b7835c5ac
maplesod <daniel.mapleson@tgac.ac.uk>
2013-07-31 17:05:20 +0100

    Updated SECT to use seqan to handle input from sequence files.
    Also implemented sequence streaming for SECT to reduce memory consumption.

--- src/sect/sect.hpp
+++ src/sect/sect.hpp
@@ -7,69 +7,225 @@
 #include <vector>
 #include <math.h>
 
+#include <seqan/sequence.h>
+#include <seqan/seq_io.h>
+
 #include <jellyfish/counter.hpp>
 #include <jellyfish/thread_exec.hpp>
 #include <jellyfish/jellyfish_helper.hpp>
 
 #include <matrix/threaded_sparse_matrix.hpp>
 
+#include "sect_args.hpp"
+
 using std::vector;
 using std::string;
 using std::cerr;
 using std::endl;
+using std::stringstream;
+
+using seqan::SequenceStream;
+using seqan::StringSet;
+using seqan::CharString;
+using seqan::Dna5String;
+
+
+#define BATCH_SIZE 1024
 
 template<typename hash_t>
 class Sect : public thread_exec
 {
 private:
-    const char                      *jfHashPath;
-    const vector<string>            *names;	    // Names of fasta sequences (in same order as seqs)
-    const vector<string>            *seqs;	    // Sequences in fasta sequences (in same order as names)
-    const uint16_t                  gc_bins;
-    const uint16_t                  cvg_bins;
-    const bool                      cvg_logscale;
-    const uint16_t                  threads;	// Number of threads to use
-    const bool                      verbose;
+
+    // Input args
+    const SectArgs                  *args;
     const size_t                    bucket_size, remaining;	// Chunking vars
 
+    // Variables that live for the lifetime of this instance
     JellyfishHelper                 *jfh;
     hash_t                          *hash;		// Jellyfish hash
+    ThreadedSparseMatrix<uint64_t>  *contamination_mx;  // Stores cumulative base count for each sequence where GC and CVG are binned
+    uint32_t                        offset;
+    uint16_t                        recordsInBatch;
+
+    // Variables that live for the lifetime of each batch
+    StringSet<CharString>           *names;
+    StringSet<Dna5String>           *seqs;
     vector<vector<uint64_t>*>       *counts;    // Kmer counts for each kmer window in sequence (in same order as seqs and names; built by this class)
     vector<float>                   *coverages; // Overall coverage calculated for each sequence from the kmer windows.
     vector<float>                   *gcs;       // GC% for each sequence
     vector<uint32_t>                *lengths;       // GC% for each sequence
-    ThreadedSparseMatrix<uint64_t>  *contamination_mx;  // Stores cumulative base count for each sequence where GC and CVG are binned
 
 public:
-    Sect(const char *_jfHashPath, const vector<string> *_names, const vector<string> *_seqs,
-        uint16_t _gc_bins, uint16_t _cvg_bins, float _cvg_logscale, uint16_t _threads, bool _verbose) :
-        jfHashPath(_jfHashPath), names(_names), seqs(_seqs),
-        gc_bins(_gc_bins), cvg_bins(_cvg_bins), cvg_logscale(_cvg_logscale),
-        threads(_threads), verbose(_verbose),
-        bucket_size(seqs->size() / threads),
-        remaining(seqs->size() % (bucket_size < 1 ? 1 : threads))
+    Sect(SectArgs *_args) :
+        args(_args),
+        bucket_size(BATCH_SIZE / args->threads_arg),
+        remaining(BATCH_SIZE % (bucket_size < 1 ? 1 : args->threads_arg))
     {
         // Setup handle to jellyfish hash
-        jfh = new JellyfishHelper(jfHashPath);
+        jfh = new JellyfishHelper(args->db_arg);
 
         // Setup space for storing output
-        counts = new vector<vector<uint64_t>*>(seqs->size());
-        coverages = new vector<float>(seqs->size());
-        gcs = new vector<float>(seqs->size());
-        lengths = new vector<uint32_t>(seqs->size());
-        contamination_mx = new ThreadedSparseMatrix<uint64_t>(gc_bins, cvg_bins, threads);
+        offset = 0;
+        recordsInBatch = 0;
+
+        contamination_mx = new ThreadedSparseMatrix<uint64_t>(args->gc_bins, args->cvg_bins, args->threads_arg);
     }
 
     ~Sect()
     {
-        // Jellyfish helper takes care of freeing the hash
-        //if (hash)
-        //    delete hash;
-
         if (jfh)
             delete jfh;
 
         jfh = NULL;
+
+
+        if (contamination_mx)
+            delete contamination_mx;
+
+        contamination_mx = NULL;
+
+        if(names)
+            delete names;
+
+        names = NULL;
+
+        if(seqs)
+            delete seqs;
+
+        seqs = NULL;
+
+        // Destroy any remaining batch variables
+        destroyBatchVars();
+    }
+
+
+    void do_it()
+    {
+        // Setup output stream for jellyfish initialisation
+        std::ostream* out_stream = args->verbose ? &cerr : (std::ostream*)0;
+
+        // Load the jellyfish hash
+        hash = jfh->loadHash(true, out_stream);
+
+        // Setup stream to sequence file and check all is well
+        SequenceStream seqStream(args->fasta_arg, SequenceStream::READ);
+        if (!isGood(seqStream))
+        {
+            std::cerr << "ERROR: Could not open the sequence file: " << args->fasta_arg << endl;
+            return;
+        }
+
+
+        // Setup output streams for files
+
+        // Sequence kmer counts output stream
+        std::ostringstream count_path;
+        count_path << args->output_prefix << "_counts.cvg";
+        ofstream_default count_path_stream(count_path.str().c_str(), cout);
+
+        // Average sequence coverage and GC% scores output stream
+        std::ostringstream cvg_gc_path;
+        cvg_gc_path << args->output_prefix << "_stats.csv";
+        ofstream_default cvg_gc_stream(cvg_gc_path.str().c_str(), cout);
+        cvg_gc_stream << "seq_name coverage gc% seq_length" << endl;
+
+
+
+        // Processes sequences in batches of records to reduce memory requirements
+        while(!atEnd(seqStream))
+        {
+            // Create object to contain records to process for this batch
+            names = new StringSet<CharString>();
+            seqs = new StringSet<Dna5String>();
+
+            // Read batch of records
+            if (readBatch(*names, *seqs, seqStream, BATCH_SIZE) != 0)
+            {
+                std::cerr << "ERROR: Could not read batch of records! Offset: " << offset << endl;
+                return;
+            }
+
+            // Record the number of records in this batch (may not be BATCH_SIZE) if we are at the end of the file
+            recordsInBatch = length(*seqs);
+
+            // Allocate memory for output produced by this batch
+            createBatchVars(recordsInBatch);
+
+            // Process batch with worker threads
+            // Process each sequence is processed in a different thread.
+            // In each thread lookup each kmer in the hash
+            exec_join(args->threads_arg);
+
+            // Output findings for this batch
+            printCounts(count_path_stream);
+            printStatTable(cvg_gc_stream);
+
+            // Remove any batch specific variables from memory
+            destroyBatchVars();
+
+            // Increment batch management vars
+            offset += recordsInBatch;
+        }
+
+
+        // Close output streams
+        count_path_stream.close();
+        cvg_gc_stream.close();
+
+        // Merge the contamination matrix
+        contamination_mx->mergeThreadedMatricies();
+
+        // Send contamination matrix to file
+        std::ostringstream contamination_mx_path;
+        contamination_mx_path << args->output_prefix << "_contamination.mx";
+        ofstream_default contamination_mx_stream(contamination_mx_path.str().c_str(), cout);
+        printContaminationMatrix(contamination_mx_stream, args->fasta_arg);
+        contamination_mx_stream.close();
+    }
+
+    void start(int th_id)
+    {
+        // Check to see if we have useful work to do for this thread, return if not
+        if (bucket_size < 1 && th_id >= recordsInBatch)
+        {
+            return;
+        }
+
+        //processInBlocks(th_id);
+        processInterlaced(th_id);
+    }
+
+
+    void printVars(std::ostream &out)
+    {
+        out << "SECT parameters:" << endl;
+        out << " - Sequence File Path: " << args->fasta_arg << endl;
+        out << " - Hash File Path: " << args->db_arg << endl;
+        out << " - Hash: " << (hash ? "loaded" : "not loaded") << endl;
+        out << " - Threads: " << args->threads_arg << endl;
+        out << " - Bucket size: " << bucket_size << endl;
+        out << " - Remaining: " << remaining << endl << endl;
+    }
+
+
+
+
+
+private:
+
+    void destroyBatchVars()
+    {
+        if(names)
+            delete names;
+
+        names = NULL;
+
+        if(seqs)
+            delete seqs;
+
+        seqs = NULL;
+
 
         if(counts)
         {
@@ -96,58 +252,22 @@ public:
             delete lengths;
 
         lengths = NULL;
-
-        if (contamination_mx)
-            delete contamination_mx;
-
-        contamination_mx = NULL;
-
     }
 
-
-    void do_it()
+    void createBatchVars(uint16_t batchSize)
     {
-        std::ostream* out_stream = verbose ? &cerr : (std::ostream*)0;
-
-        // Load the jellyfish hash
-        hash = jfh->loadHash(true, out_stream);
-
-        // Process each fasta sequence in a different thread.
-        // In each thread lookup each kmer in the hash
-        exec_join(threads);
-
-        contamination_mx->mergeThreadedMatricies();
-    }
-
-    void start(int th_id)
-    {
-        // Check to see if we have useful work to do for this thread, return if not
-        if (bucket_size < 1 && th_id >= seqs->size())
-        {
-            return;
-        }
-
-        //processInBlocks(th_id);
-        processInterlaced(th_id);
-    }
-
-
-    void printVars(std::ostream &out)
-    {
-        out << "SECT parameters:" << endl;
-        out << " - Hash: " << (hash ? "present" : "not specified") << endl;
-        out << " - Sequences to process: " << seqs->size() << endl;
-        out << " - Threads: " << threads << endl;
-        out << " - Bucket size: " << bucket_size << endl;
-        out << " - Remaining: " << remaining << endl << endl;
+        counts = new vector<vector<uint64_t>*>(batchSize);
+        coverages = new vector<float>(batchSize);
+        gcs = new vector<float>(batchSize);
+        lengths = new vector<uint32_t>(batchSize);
     }
 
 
     void printCounts(std::ostream &out)
     {
-        for(int i = 0; i < names->size(); i++)
+        for(int i = 0; i < recordsInBatch; i++)
         {
-            out << ">" << (*names)[i].c_str() << endl;
+            out << ">" << toCString((*names)[i]) << endl;
 
             vector<uint64_t>* seqCounts = (*counts)[i];
 
@@ -171,26 +291,22 @@ public:
 
     void printStatTable(std::ostream &out)
     {
-        out << "seq_name coverage gc% seq_length" << endl;
-        for(int i = 0; i < names->size(); i++)
+        for(int i = 0; i < recordsInBatch; i++)
         {
             out << (*names)[i] << " " << (*coverages)[i] << " " << (*gcs)[i] << " " << (*lengths)[i] << endl;
         }
     }
 
     // Print kmer comparison matrix
-    void printContaminationMatrix(std::ostream &out)
+    void printContaminationMatrix(std::ostream &out, const char* seqFile)
     {
-        out << "# Fasta file processed: " << "???" << endl;
-        out << "# Jellyfish hash: " << jfHashPath << endl;
-        out << "# Each column represents the GC%, with " << gc_bins << " bins. First bin is 0% and last bin in 100%" << endl;
-        out << "# Each row represents the sequence coverage, with " << cvg_bins << " bins.  First bin in 0x last bin in " << endl;
+        out << "# Sequence file processed: " << seqFile << endl;
+        out << "# Jellyfish hash: " << args->db_arg << endl;
+        out << "# Each column represents the GC%, with " << args->gc_bins << " bins. First bin is 0% and last bin in 100%" << endl;
+        out << "# Each row represents the sequence coverage, with " << args->cvg_bins << " bins.  First bin in 0x last bin in " << endl;
 
         contamination_mx->getFinalMatrix()->printMatrix(out);
     }
-
-
-private:
 
     // This method won't be optimal in most cases... Fasta files are normally sorted by length (largest first)
     // So first thread will be asked to do more work than the rest
@@ -206,7 +322,7 @@ private:
         // Process a remainder if required
         if (th_id < remaining)
         {
-            size_t rem_idx = (threads * bucket_size) + th_id;
+            size_t rem_idx = (args->threads_arg * bucket_size) + th_id;
             processSeq(rem_idx);
         }
     }
@@ -215,8 +331,8 @@ private:
     void processInterlaced(uint_t th_id)
     {
         size_t start = th_id;
-        size_t end = seqs->size();
-        for(size_t i = start; i < end; i+=threads)
+        size_t end = recordsInBatch;
+        for(size_t i = start; i < end; i += args->threads_arg)
         {
             processSeq(i, th_id);
         }
@@ -225,17 +341,26 @@ private:
     void processSeq(const size_t index, const uint_t th_id)
     {
         uint_t kmer = hash->get_mer_len();
-        string seq = (*seqs)[index];
-        uint_t nbCounts = seq.length() - kmer + 1;
+
+        // There's no substring functionality in SeqAn in this version (1.4.1).  So we'll just
+        // use regular c++ string's for this bit.  The next version of SeqAn may offer substring
+        // functionality, at which time I might change this code to make it run faster using
+        // SeqAn's datastructures.
+        stringstream ssSeq;
+        ssSeq << (*seqs)[index];
+        string seq = ssSeq.str();
+
+        uint16_t seqLength = seq.length();
+        uint_t nbCounts = seqLength - kmer + 1;
         float mean_cvg = 0;
 
-        if (seq.length() < kmer)
+        if (seqLength < kmer)
         {
-            cerr << (*names)[index].c_str() << ": " << seq << "  is too short to compute coverage.  Setting sequence coverage to 0." << endl;
+            cerr << (*names)[index] << ": " << seq << " is too short to compute coverage.  Sequence length is "
+                 << seqLength << " and kmer length is " << kmer << ". Setting sequence coverage to 0." << endl;
         }
         else
         {
-
             vector<uint_t>* seqCounts = new vector<uint_t>(nbCounts);
 
             uint64_t sum = 0;
@@ -257,6 +382,7 @@ private:
 
                     (*seqCounts)[i] = count;
                 }
+
             }
 
             (*counts)[index] = seqCounts;
@@ -264,17 +390,18 @@ private:
             // Assumes simple mean calculation for sequence coverage for now... plug in Bernardo's method later.
             mean_cvg = (float)sum / (float)nbCounts;
             (*coverages)[index] = mean_cvg;
+
         }
 
         // Add length
-        (*lengths)[index] = seq.length();
+        (*lengths)[index] = seqLength;
 
         // Calc GC%
         uint32_t gs = 0;
         uint32_t cs = 0;
         uint32_t ns = 0;
 
-        for(uint32_t i = 0; i < seq.length(); i++)
+        for(uint32_t i = 0; i < seqLength; i++)
         {
             char c = seq[i];
 
@@ -286,19 +413,20 @@ private:
                 ns++;
         }
 
-        float gc_perc = ((float)(gs + cs)) / ((float)(seq.length() - ns));
+        float gc_perc = ((float)(gs + cs)) / ((float)(seqLength - ns));
         (*gcs)[index] = gc_perc;
 
-        float log_cvg = cvg_logscale ? log10(mean_cvg) : mean_cvg;
+        float log_cvg = args->cvg_logscale ? log10(mean_cvg) : mean_cvg;
 
         // Assume log_cvg 5 is max value
-        float compressed_cvg = cvg_logscale ? log_cvg * (cvg_bins / 5.0) : mean_cvg * 0.1;
+        float compressed_cvg = args->cvg_logscale ? log_cvg * (args->cvg_bins / 5.0) : mean_cvg * 0.1;
 
-        uint16_t x = gc_perc * gc_bins;  // Convert float to 1.dp
-        uint16_t y = compressed_cvg >= cvg_bins ? cvg_bins - 1 : compressed_cvg;      // Simply cap the y value
+        uint16_t x = gc_perc * args->gc_bins;  // Convert float to 1.dp
+        uint16_t y = compressed_cvg >= args->cvg_bins ? args->cvg_bins - 1 : compressed_cvg;      // Simply cap the y value
 
         // Add bases to matrix
-        contamination_mx->getThreadMatrix(th_id)->inc(x, y, seq.length());
+        contamination_mx->getThreadMatrix(th_id)->inc(x, y, seqLength);
+
     }
 
 };
--- src/sect/sect_main.cc
+++ src/sect/sect_main.cc
@@ -10,10 +10,6 @@
 #include <fstream>
 #include <vector>
 
-#include <jellyfish/mer_counting.hpp>
-
-#include <kseq/kseq_helper.h>
-
 #include "sect.hpp"
 #include "sect_args.hpp"
 #include "sect_main.hpp"
@@ -34,13 +30,8 @@ int sectStart(int argc, char *argv[])
     if (args.verbose)
         args.print();
 
-    // Load enitre fasta file into memory
-    vector<string> names;
-    vector<string> seqs;
-    readFasta(args.fasta_arg, names, seqs, args.verbose);
-
     // Create the sequence coverage object
-    Sect<hash_query_t> sect(args.db_arg, &names, &seqs, args.gc_bins, args.cvg_bins, args.cvg_logscale, args.threads_arg, args.verbose);
+    Sect<hash_query_t> sect(&args);
 
     // Output seqcvg parameters to stderr if requested
     if (args.verbose)
@@ -48,28 +39,6 @@ int sectStart(int argc, char *argv[])
 
     // Do the work
     sect.do_it();
-
-    // Send sequence kmer counts to file
-    std::ostringstream count_path;
-    count_path << args.output_prefix << "_counts.cvg";
-    ofstream_default count_path_stream(count_path.str().c_str(), cout);
-    sect.printCounts(count_path_stream);
-    count_path_stream.close();
-
-
-    // Send average sequence coverage and GC% scores to file
-    std::ostringstream cvg_gc_path;
-    cvg_gc_path << args.output_prefix << "_stats.csv";
-    ofstream_default cvg_gc_stream(cvg_gc_path.str().c_str(), cout);
-    sect.printStatTable(cvg_gc_stream);
-    cvg_gc_stream.close();
-
-    // Send contamination matrix to file
-    std::ostringstream contamination_mx_path;
-    contamination_mx_path << args.output_prefix << "_contamination.mx";
-    ofstream_default contamination_mx_stream(contamination_mx_path.str().c_str(), cout);
-    sect.printContaminationMatrix(contamination_mx_stream);
-    contamination_mx_stream.close();
 
     return 0;
 }
