2162467ed1a645782cc683e7db03d4f829248882
Daniel Mapleson <daniel.mapleson@tgac.ac.uk>
2013-11-08 17:41:36 +0000

    Overhauled the way we use autotools.  Hopefully these changes should make the install procedure more robust than before.

--- clean.sh
+++ clean.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 
-rm -r -f autom4te.cache autoscan.* Makefile.in aclocal.m4 configure depcomp INSTALL install-sh missing config.* *.scan stamp-h1 */*.o
 
+rm -r -f autom4te.cache build-aux autoscan.* Makefile Makefile.in aclocal.m4 configure config.* *.scan stamp-h1 */*.o *~ kat-* clean
 
 
