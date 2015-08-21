9f332d1459433a4305febe1c5190f9151147b86c
Daniel Mapleson <daniel.mapleson@tgac.ac.uk>
2015-08-21 14:46:05 +0100

    Fixed a bug which meant we can't handle non canonical bases in sequences passed to sect.  Bumped version to 2.0.3

--- src/sect.cc
+++ src/sect.cc
@@ -85,9 +85,9 @@ void kat::Sect::execute() {
     input.validateInput();
     
     // Create output directory
-    path parentDir = outputPrefix.parent_path();
-    if (!exists(parentDir) || !is_directory(parentDir)) {
-        if (!create_directories(parentDir)) {
+    path parentDir = bfs::canonical(outputPrefix.parent_path());
+    if (!bfs::exists(parentDir) || !bfs::is_directory(parentDir)) {
+        if (!bfs::create_directories(parentDir)) {
             BOOST_THROW_EXCEPTION(SectException() << SectErrorInfo(string(
                     "Could not create output directory: ") + parentDir.string()));
         }
