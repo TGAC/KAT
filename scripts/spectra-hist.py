36beb1a76c49087a0c91e30b98692c067870309a
George Kettleborough (TGAC) <george.kettleborough@tgac.ac.uk>
2015-11-10 14:19:23 +0000

    Implement spectra-hist

--- scripts/spectra-hist.py
+++ scripts/spectra-hist.py
@@ -4,35 +4,38 @@ import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 
+from findpeaks import *
+from header import *
+
 # ----- command line parsing -----
 parser = argparse.ArgumentParser(
     description="Creates K-mer spectra plot from one of more histograms.")
 
-parser.add_argument("histo_file", type=str, nargs='+',
+parser.add_argument("histo_files", type=str, nargs='+',
                     help="The input histogram file from KAT")
 
-parser.add_argument("-p", "--output_type", type=str, default="png",
+parser.add_argument("-p", "--output_type", type=str,
                     choices=["png", "pdf", "eps"],
                     help="The plot file type to create.")
 parser.add_argument("-o", "--output", type=str, default="kat-spectra-hist",
                     help="The path to the output file.")
-parser.add_argument("-t", "--title", type=str, default="Spectra Copy Number Plot",
+parser.add_argument("-t", "--title", type=str,
                     help="Title for plot")
-parser.add_argument("-a", "--x_label", type=str, default="X",
+parser.add_argument("-a", "--x_label", type=str,
                     help="Label for x-axis")
-parser.add_argument("-b", "--y_label", type=str, default="Y",
+parser.add_argument("-b", "--y_label", type=str,
                     help="Label for y-axis")
 parser.add_argument("-r", "--x_min", type=int, default=0,
                     help="Minimum value for x-axis")
 parser.add_argument("-s", "--y_min", type=int, default=0,
                     help="Minimum value for y-axis")
-parser.add_argument("-x", "--x_max", type=int, default=1000,
+parser.add_argument("-x", "--x_max", type=int,
                     help="Maximum value for x-axis")
-parser.add_argument("-y", "--y_max", type=int, default=1000,
+parser.add_argument("-y", "--y_max", type=int,
                     help="Maximum value for y-axis")
-parser.add_argument("-w", "--width", type=int, default=1024,
+parser.add_argument("-w", "--width", type=int, default=8,
                     help="Width of canvas")
-parser.add_argument("-l", "--height", type=int, default=1024,
+parser.add_argument("-l", "--height", type=int, default=6,
                     help="Height of canvas")
 parser.add_argument("-m", "--x_logscale", dest="x_logscale", action="store_true",
                     help="X-axis is logscale. Overrides x_min and x_max")
@@ -47,3 +50,96 @@ parser.set_defaults(verbose=False)
 args = parser.parse_args()
 # ----- end command line parsing -----
 
+if args.verbose:
+    print "Input files given: {:d}".format(len(args.histo_files))
+
+headers = []
+x = []
+y = []
+for histo_file in args.histo_files:
+    input_file = open(histo_file)
+
+    header = readheader(input_file)
+
+    matrix = np.loadtxt(input_file)
+    input_file.close()
+    if args.verbose:
+        print "{:d} by {:d} matrix file loaded.".format(matrix.shape[0],
+                                                        matrix.shape[1])
+    headers.append(header)
+    x.append(matrix[:,0])
+    y.append(matrix[:,1])
+
+if args.title is not None:
+    title = args.title
+elif "Title" in header:
+    title = headers[0]["Title"]
+else:
+    title = "Spectra Copy Number Plot"
+
+if args.x_label is not None:
+    x_label = args.x_label
+elif "XLabel" in header:
+    x_label = headers[0]["XLabel"]
+else:
+    x_label = "X"
+
+if args.y_label is not None:
+    y_label = args.y_label
+elif "YLabel" in header:
+    y_label = headers[0]["YLabel"]
+else:
+    y_label = "Y"
+
+# find limits
+if args.x_max is None or args.y_max is None:
+    xmax = map(len, x)
+    ysum = map(np.sum, y)
+    ymax = map(np.max, y)
+    for i in range(len(x)):
+        peakx = findpeaks(y[i])
+        peakx = peakx[peakx != 1]
+        peaky = y[i][peakx]
+
+        for j in range(1, xmax[i], xmax[i]/1000 + 1):
+            if np.sum(y[i][:j]) >= ysum[i] * 0.999:
+                xmax[i] = j
+                break
+
+        ymax[i] = np.max(peaky) * 1.1
+
+    xmax = max(xmax)
+    ymax = max(ymax)
+    if args.verbose:
+        print "Automatically detected axis limits:"
+        print "xmax: ", xmax
+        print "ymax: ", ymax
+
+if args.x_max is not None:
+    xmax = args.x_max
+if args.y_max is not None:
+    ymax = args.y_max
+
+plt.figure(num = None, figsize=(args.width, args.height))
+
+for x,y in zip(x,y):
+    plt.plot(x, y)
+
+if args.x_logscale:
+    plt.xscale("log")
+if args.y_logscale:
+    plt.yscale("log")
+
+plt.axis([args.x_min, xmax, args.y_min, ymax])
+
+plt.title(title)
+plt.xlabel(x_label)
+plt.ylabel(y_label)
+plt.grid(True, color="black", alpha=0.2)
+
+if args.output_type is not None:
+    output_name = args.output + '.' + args.output_type
+else:
+    output_name = args.output
+
+plt.savefig(output_name, dpi=300)
