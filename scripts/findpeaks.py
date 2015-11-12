4d4b428875ee9b06cab0535c527eae9d5e20f5f3
George Kettleborough (TGAC) <george.kettleborough@tgac.ac.uk>
2015-11-12 16:23:29 +0000

    Make findpeaks work with matrix input

--- scripts/findpeaks.py
+++ scripts/findpeaks.py
@@ -3,8 +3,9 @@
 import numpy as np
 
 def findpeaks(a):
+    a = np.squeeze(np.asarray(a))
     ad = np.sign(np.diff(a))
     # remove zeros to find end of plateaus
     ad[ad == 0] = 1
-    return np.where(np.diff(ad) == -2)[0]
+    return np.where(np.diff(ad) == -2)[0] + 1
 
