2406802a89a152f52686a4302fb4ca40b0a200af
Daniel Mapleson <daniel.mapleson@tgac.ac.uk>
2016-07-18 14:01:50 +0100

    Forcing sphinx dependency to 1.4 and changing math extension module to imgmath, as previous pngmath was deprecated.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -29,8 +29,7 @@ needs_sphinx = '1.3'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.pngmath',
-    'sphinx.ext.mathjax',
+    'sphinx.ext.imgmath'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
