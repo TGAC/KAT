0d395912c371dc9d95a1fc8b731e70f763650dcb
Daniel Mapleson <daniel.mapleson@tgac.ac.uk>
2016-11-04 14:39:27 +0000

    Implemented paired end filtering for "kat filter seq".  Unfortunately, I had to modify the interface for this slightly so I've bumped the minor revision.

--- src/filter_sequence.cc
+++ src/filter_sequence.cc
@@ -67,37 +67,23 @@ using kat::KatFS;
 using kat::PlotDensity;
 
 #include "filter_sequence.hpp"
+#include "comp.hpp"
 
 
 
 
-unique_ptr<kat::filter::SeqFilterCounter> kat::filter::ThreadedSeqStatsCounters::merge() {
-    
-    unique_ptr<kat::filter::SeqFilterCounter> merged( new SeqFilterCounter() );
-    
-    for(const auto& c : counters) {        
-        for(const auto& s : c.seq_stats) {
-            merged->add(s);
-        }
-    }
-
-    // Sort by index
-    merged->sort();
-        
-    return merged;
-}
-
-
-kat::filter::FilterSeq::FilterSeq(const path& _seq_file, const path& _input) {
-    this->seq_file = _seq_file;
+kat::filter::FilterSeq::FilterSeq(const path& _seq_file_1, const path& _seq_file_2, const path& _input) {
+    this->seq_file_1 = _seq_file_1;
+    this->seq_file_2 = _seq_file_2;
     vector<path> vecInput;
     vecInput.push_back(_input);
     init(vecInput);
 }
     
 
-kat::filter::FilterSeq::FilterSeq(const path& _seq_file, const vector<path>& _input) {
-    this->seq_file = _seq_file;
+kat::filter::FilterSeq::FilterSeq(const path& _seq_file_1, const path& _seq_file_2, const vector<path>& _input) {
+    this->seq_file_1 = _seq_file_1;
+    this->seq_file_2 = _seq_file_2;
     init(_input);
 }
 
@@ -114,24 +100,45 @@ void kat::filter::FilterSeq::init(const vector<path>& _input) {
     invert = DEFAULT_FILT_SEQ_INVERT;
     separate = DEFAULT_FILT_SEQ_SEPARATE;   
     doStats = false;
+    
+    keepers = 0;
+    total = 0;
 }
 
 void kat::filter::FilterSeq::execute() {
     
-    if (bfs::is_symlink(seq_file)) {
-        if (bfs::symbolic_link_exists(seq_file)) {
-            seq_file = bfs::canonical(seq_file);
+    if (bfs::is_symlink(seq_file_1)) {
+        if (bfs::symbolic_link_exists(seq_file_1)) {
+            seq_file_1 = bfs::canonical(seq_file_1);
         }
         else {
             BOOST_THROW_EXCEPTION(FilterSeqException() << FilterSeqErrorInfo(string(
-                "Could not find input file at: ") + seq_file.string() + "; please check the path and try again."));
+                "Could not find input file at: ") + seq_file_1.string() + "; please check the path and try again."));
         }
     }
 
-    if (!bfs::exists(seq_file)) {
+    if (!bfs::exists(seq_file_1)) {
         BOOST_THROW_EXCEPTION(FilterSeqException() << FilterSeqErrorInfo(string(
-                "Could not find input file at: ") + seq_file.string() + "; please check the path and try again."));
+                "Could not find input file at: ") + seq_file_1.string() + "; please check the path and try again."));
     }
+
+    if (this->isPaired()) {
+        if (bfs::is_symlink(seq_file_2)) {
+            if (bfs::symbolic_link_exists(seq_file_2)) {
+                seq_file_2 = bfs::canonical(seq_file_2);
+            }
+            else {
+                BOOST_THROW_EXCEPTION(FilterSeqException() << FilterSeqErrorInfo(string(
+                    "Could not find sequence file at: ") + seq_file_2.string() + "; please check the path and try again."));
+            }
+        }
+
+        if (!bfs::exists(seq_file_2)) {
+            BOOST_THROW_EXCEPTION(FilterSeqException() << FilterSeqErrorInfo(string(
+                    "Could not find sequence file at: ") + seq_file_2.string() + "; please check the path and try again."));
+        }
+    }
+
 
     // Validate input
     input.validateInput();
@@ -148,118 +155,167 @@ void kat::filter::FilterSeq::execute() {
         input.loadHeader();
         input.loadHash();                
     }
+       
     
-    // Resize all the counters to the requested number of threads
-    stats.resize(threads);
-    
-    // Do the core of the work here
+    // Do the work
     processSeqFile();
     
-    // Merge (and print) results
-    unique_ptr<SeqFilterCounter> merged_stats = stats.merge();
-    
-    cout << "Found " << merged_stats->calcKeepers(threshold) << " / " << merged_stats->size() << " to keep" << endl << endl;
+    // Summary
+    cout << "Found " << keepers << " / " << total << " to keep" << endl << endl;
 
-    // Filter seqs
-    save(merged_stats->seq_stats);
 }
+
 
 
 void kat::filter::FilterSeq::processSeqFile() {
     
     auto_cpu_timer timer(1, "  Time taken: %ws\n\n");     
     
-    cout << "Analysing sequences ...";
-    cout.flush();
+    cout << "Filtering sequences ..." << endl;
     
-    bucket_size = BATCH_SIZE / threads;
-    remaining = BATCH_SIZE % (bucket_size < 1 ? 1 : threads);
+    // Temporary storage for sequence data
+    reader = unique_ptr<seqan::SeqFileIn>(new seqan::SeqFileIn(seq_file_1.c_str()));
     
-    // Setup space for storing output
-    offset = 0;
-    recordsInBatch = 0;
-
-    names = seqan::StringSet<seqan::CharString>();
-    seqs = seqan::StringSet<seqan::CharString>();
+    if (this->isPaired()) {
+        reader2 = unique_ptr<seqan::SeqFileIn>(new seqan::SeqFileIn(seq_file_2.c_str()));
+    }
     
-    // Open file, create RecordReader and check all is well
-    seqan::SeqFileIn reader(seq_file.c_str());
+    // Setup output file for statistics and output header if requested
+    if (doStats) {
+        path stats_path_out(output_prefix.string() + ".stats");
+        stats_stream = unique_ptr<ofstream>(new ofstream(stats_path_out.c_str()));
+        (*stats_stream) << "index\tnb_bases\tnb_kmers\tnb_hits\tratio" << endl;
+    }
     
+    // Setup file paths
+    path ext = seq_file_1.extension();
+    
+    path output_path_in(output_prefix.string() + ".in" + (this->isPaired() ? ".R1" : "") + ext.string());
+    inWriter = unique_ptr<seqan::SeqFileOut>(new seqan::SeqFileOut(output_path_in.c_str()));    
+    if (separate) {
+        path output_path_out(output_prefix.string() + ".out" + (this->isPaired() ? ".R1" : "") + ext.string());
+        outWriter = unique_ptr<seqan::SeqFileOut>(new seqan::SeqFileOut(output_path_out.c_str()));    
+    }
+    
+    
+    if (this->isPaired()) {
+        path output_path_in2(output_prefix.string() + ".in.R2" + ext.string());
+        inWriter2 = unique_ptr<seqan::SeqFileOut>(new seqan::SeqFileOut(output_path_in2.c_str()));
+        if (separate) {
+            path output_path_out2(output_prefix.string() + ".out.R2" + ext.string());
+            outWriter2 = unique_ptr<seqan::SeqFileOut>(new seqan::SeqFileOut(output_path_out2.c_str()));
+        }
+    }
+        
     // Processes sequences in batches of records to reduce memory requirements
-    while (!seqan::atEnd(reader)) {
-        if (verbose)
-            cout << "Loading Batch of sequences... ";
-
-        seqan::clear(names);
-        seqan::clear(seqs);
-
-        seqan::readRecords(names, seqs, reader, BATCH_SIZE);
-
-        recordsInBatch = seqan::length(names);
-
-        if (verbose)
-            cout << "Loaded " << recordsInBatch << " records.  Processing batch... ";
-
+    uint64_t index = 0;
+    while (!seqan::atEnd(*reader)) {
+        
+        seqan::readRecord(name, seq, qual, *reader);
+            
+        if (this->isPaired()) {
+            seqan::readRecord(name2, seq2, qual2, *reader2);
+        }
+        
         // Process batch with worker threads
         // Process each sequence is processed in a different thread.
         // In each thread lookup each K-mer in the hash
-        analyseBatch();
+        processSeq(index++);
+        
+        if (index % 100000 == 0) {
+            cout << "Processed " << index << (this->isPaired() ? " pairs" : " entries") << endl;
+        }
 
-        // Increment batch management vars
-        offset += recordsInBatch;
-
-        if (verbose)
-            cout << "done" << endl;
+    }
+    
+    if (this->isPaired() && !seqan::atEnd(*reader2)) {
+        BOOST_THROW_EXCEPTION(FilterSeqException() << FilterSeqErrorInfo(string(
+                    "Second sequence file appears to be longer than the first.")));
     }
 
-    seqan::close(reader);
+    seqan::close(*reader);    
     
-    cout << " done.";
+    seqan::close(*inWriter);
+    if (separate) {
+        seqan::close(*outWriter);
+    }
+    
+    if (this->isPaired()) {
+        seqan::close(*reader2);
+        seqan::close(*inWriter2);
+        if (separate) {
+            seqan::close(*outWriter2);
+        }
+    }
+    
+    if (this->doStats) {
+        stats_stream->close();        
+    }
+    
+    cout << "Finished filtering.";
     cout.flush();
 }
 
 
-void kat::filter::FilterSeq::analyseBatch() {
+void kat::filter::FilterSeq::processSeq(uint64_t index) {
 
-    vector<thread> t(threads);
-
-    for(uint16_t i = 0; i < threads; i++) {
-        t[i] = thread(&FilterSeq::analyseBatchSlice, this, i);
-    }
-
-    for(uint16_t i = 0; i < threads; i++){
-        t[i].join();
-    }            
-}
-
-void kat::filter::FilterSeq::analyseBatchSlice(int th_id) {
-    // Check to see if we have useful work to do for this thread, return if not
-    if (bucket_size < 1 && th_id >= recordsInBatch) {
-        return;
+    
+    vector<bool> kFound;
+    this->getProfile(seq, kFound);
+    
+    if (this->isPaired()) {
+        vector<bool> kFound2;
+        this->getProfile(seq2, kFound2);
+        kFound.insert(kFound.end(), kFound2.begin(), kFound2.end());
     }
     
-    for (size_t i = th_id; i < recordsInBatch; i += threads) {
-        processSeq(i, th_id);
+    uint32_t nbFound = 0;
+    for(const auto& b : kFound) {
+        if (b) nbFound ++;
     }
+    
+    SeqStats stats(index, nbFound, kFound.size());  
+    
+    double ratio = stats.calcRatio();
+
+    if ((ratio >= threshold && !invert) || (invert && ratio < threshold)) {
+        keepers++;
+        seqan::writeRecord(*inWriter, name, seq, qual);
+        if (this->isPaired()) {
+            seqan::writeRecord(*inWriter2, name2, seq2, qual2);
+        }
+    }
+    else if (separate) {
+        seqan::writeRecord(*outWriter, name, seq, qual);
+        if (this->isPaired()) {
+            seqan::writeRecord(*outWriter2, name2, seq2, qual2);
+        }
+    }
+
+    if (doStats) {
+        size_t len = this->isPaired() ? seqan::length(seq) + seqan::length(seq2) : seqan::length(seq);
+        
+        (*stats_stream) << index << "\t" << len << "\t" << stats.nb_kmers 
+                << "\t" << stats.matches << "\t" << ratio << endl;
+    }
+    
+    total++;
 }
 
-
-void kat::filter::FilterSeq::processSeq(const size_t index, const uint16_t th_id) {
-
+void kat::filter::FilterSeq::getProfile(seqan::CharString& sequence, vector<bool>& hits) {
     // There's no substring functionality in SeqAn in this version (2.0.0).  So we'll just
     // use regular c++ string's for this bit.  This conversion of strings:
     // {CharString -> c++ string -> substring -> jellyfish mer_dna} is 
     // inefficient. Reducing the number of conversions necessary will 
     // make a big performance improvement here
     stringstream ssSeq;
-    ssSeq << seqs[index];
-    string seq = ssSeq.str();
-
-    uint64_t seqLength = seq.length();
+    ssSeq << sequence;
+    string s = ssSeq.str();
+    
+    uint64_t seqLength = s.length();
     uint64_t nbCounts = seqLength - input.merLen + 1;
     uint64_t nbInvalid = 0;
     
-    vector<bool> kFound(nbCounts, 0);
     
     if (nbCounts <= 0) {
 
@@ -271,109 +327,27 @@ void kat::filter::FilterSeq::processSeq(const size_t index, const uint16_t th_id
 
         for (uint64_t i = 0; i < nbCounts; i++) {
 
-            string merstr = seq.substr(i, input.merLen);
-
+            string merstr = s.substr(i, input.merLen);
+            
             // Jellyfish compacted hash does not support Ns so if we find one set this kmer to false
             if (!validKmer(merstr)) {
-                kFound[i] = false;
+                hits.push_back(false);
                 nbInvalid++;
             } else {                
                 mer_dna mer(merstr);
                 uint64_t count = JellyfishHelper::getCount(input.hash, mer, input.canonical);
-                kFound[i] = count > 0;
+                hits.push_back(count > 0);
             }
         }
     }
-
-    uint32_t nbFound = 0;
-    for(const auto& b : kFound) {
-        if (b) nbFound ++;
-    }
-    
-    unique_ptr<SeqStats> k( new SeqStats(offset+index, nbFound, nbCounts) );        
-    stats.add(th_id, std::move(k));
 }
 
-void kat::filter::FilterSeq::save(vector<shared_ptr<SeqStats> >& stats) {
-    
-    if (stats.empty()) {
-        cout << "Nothing to filter" << endl << endl;
-    }
-    else {
-    
-        auto_cpu_timer timer(1, "  Time taken: %ws\n\n");     
-
-        path ext = seq_file.extension();
-        path output_path_in(output_prefix.string() + ".in" + ext.string());
-        path output_path_out(output_prefix.string() + ".out" + ext.string());
-        path stats_path_out(output_prefix.string() + ".stats");
-
-        cout << "Saving kept sequences to " << output_path_in.string() << " ...";
-        cout.flush();
-
-        // Open file, create RecordReader and check all is well
-        // Open file and create RecordReader.
-        seqan::SeqFileIn reader(seq_file.c_str());
-        seqan::SeqFileOut inWriter(output_path_in.c_str());
-        seqan::SeqFileOut outWriter(output_path_out.c_str());
-        
-        ofstream stats_stream(stats_path_out.c_str());
-        
-        if (doStats) {
-            stats_stream << "header\tnb_bases\tnb_kmers\tnb_hits\tratio" << endl;
-        }
-
-        // Processes sequences in batches of records to reduce memory requirements
-        uint32_t i = 0;
-        
-        // Loop until end
-        while (!atEnd(reader)) {
-
-            // Read record
-            seqan::CharString id;
-            seqan::Dna5String seq;
-            seqan::CharString qual;
-            seqan::readRecord(id, seq, qual, reader);
-            
-            double ratio = stats[i]->calcRatio();
-
-            if ((ratio >= threshold && !invert) || (invert && ratio < threshold)) {
-                seqan::writeRecord(inWriter, id, seq, qual);
-            }
-            else if (separate) {
-                seqan::writeRecord(outWriter, id, seq, qual);
-            }
-
-            if (doStats) {
-                stats_stream << id << "\t" << seqan::length(seq) << "\t" << stats[i]->nb_kmers 
-                        << "\t" << stats[i]->matches << "\t" << ratio << endl;
-            }
-            
-            i++;
-        }
-
-        seqan::close(reader);
-        seqan::close(inWriter);
-        seqan::close(outWriter);
-        stats_stream.close();  
-
-        if (!separate) {
-            boost::filesystem::remove(output_path_out);
-        }
-        
-        if (!doStats) {
-            boost::filesystem::remove(stats_path_out);
-        }
-
-        cout << " done.";
-        cout.flush();
-    }
-}
 
 int kat::filter::FilterSeq::main(int argc, char *argv[]) {
 
     vector<path>    inputs;
-    path            seq_file;
+    path            seq_file_1;
+    path            seq_file_2;
     path            output_prefix;
     uint16_t        threads;
     double          threshold;
@@ -400,11 +374,15 @@ int kat::filter::FilterSeq::main(int argc, char *argv[]) {
             ("threshold,T", po::value<double>(&threshold)->default_value(DEFAULT_FILT_SEQ_THRESHOLD),
                 "What percentage of the sequence needs to be covered with target k-mers to keep the sequence")
             ("invert,i", po::bool_switch(&invert)->default_value(false),
-                "Whether to take k-mers outside region as selected content, rather than those inside.")
+                "Whether to output sequences not found in the kmer hash, rather than those inside.")
             ("separate,s", po::bool_switch(&separate)->default_value(false),
-                "Whether to partition the k-mers into two sets, those inside region and those outside.  Works in combination with \"invert\".")
+                "Whether to partition the sequences into two sets, those with k-mers detected and those without.  Works in combination with \"invert\".")
+            ("seq", po::value<path>(&seq_file_1),
+                "The sequence file to filter")
+            ("seq2", po::value<path>(&seq_file_2),
+                "The second sequence file to filter (use this if you want to filter paired end reads)")
             ("stats", po::bool_switch(&stats)->default_value(false),
-                "Whether to emit statistics about quantity of found k-mers in each sequence.")
+                "Whether to emit statistics about quantity of found k-mers in each sequence.  If the user specifies seq2, then each entry will represent both sequences combined.")
             ("non_canonical,N", po::bool_switch(&non_canonical)->default_value(false),
                 "If counting fast(a/q) input, this option specifies whether the jellyfish hash represents K-mers produced for both strands (canonical), or only the explicit kmer found.")
             ("mer_len,m", po::value<uint16_t>(&mer_len)->default_value(DEFAULT_MER_LEN),
@@ -420,13 +398,11 @@ int kat::filter::FilterSeq::main(int argc, char *argv[]) {
     // in config file, but will not be shown to the user.
     po::options_description hidden_options("Hidden options");
     hidden_options.add_options()
-            ("seq_file", po::value<path>(&seq_file), "Path to file containing sequences to filter.")
             ("inputs", po::value<std::vector<path>>(&inputs), "Path to the input file(s) to process.")
             ;
 
     // Positional option for the input bam file
     po::positional_options_description p;
-    p.add("seq_file", 1);
     p.add("inputs", -1);
 
     // Combine non-positional options
@@ -443,14 +419,20 @@ int kat::filter::FilterSeq::main(int argc, char *argv[]) {
         cout << generic_options << endl;
         return 1;
     }
+    
+    if (seq_file_1.empty()) {
+        BOOST_THROW_EXCEPTION(FilterSeqException() << FilterSeqErrorInfo(string(
+                    "You must specify at least one sequence file to filter")));
+    }
 
     auto_cpu_timer timer(1, "KAT filter seq completed.\nTotal runtime: %ws\n\n");        
 
     cout << "Running KAT in filter sequence mode" << endl
          << "-----------------------------------" << endl << endl;
 
+    
     // Create the sequence coverage object
-    FilterSeq filter(seq_file, inputs);
+    FilterSeq filter(seq_file_1, seq_file_2, inputs);
     filter.setThreshold(threshold);
     filter.setOutput_prefix(output_prefix);
     filter.setThreads(threads);
