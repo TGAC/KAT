406d7fa866b4edc06bf1f611b561c49d491c43dc
Daniel Mapleson <daniel.mapleson@tgac.ac.uk>
2016-12-08 08:59:43 +0000

    Moving rt dependency out of boost variable group so it's on its own now.
    Fixed an issue I just introduced with travis gcc5 option.

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -7,11 +7,11 @@ else
     sudo apt-get update -qq
     sudo apt-get install -qq libc6-dev
     if [ "$COMPILER" == "GCC5" ]; then 
-        sudo apt-get install -qq gcc-5.3 g++-5.3
-        sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-5.3 100
-        sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5.3 100
-        export CXX="g++-5.3"
-        export CC="gcc-5.3"
+        sudo apt-get install -qq gcc-5 g++-5
+        sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-5 100
+        sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 100
+        export CXX="g++-5"
+        export CC="gcc-5"
     else 
         sudo apt-get install -qq gcc-4.9 g++-4.9
         sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.9 100
