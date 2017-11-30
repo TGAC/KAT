d853a6a061a7efaf295430bb6dcda13afafa63a5
Daniel Mapleson <daniel.mapleson@earlham.ac.uk>
2017-11-30 17:14:51 +0000

    Changing version to 2.4.0.
    Tidying up travis files.
    
    Updating NEWS file with latest changes.

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
-    # Install boost -- looks like boost 1.55.0_2 is already installed with brew
-    #brew install boost
-
 
     # install anaconda
     wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh;
@@ -18,7 +15,7 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 else
 
     # Plotting installation
-    if [[ "$PLOT" == "python" ]]; then 
+    if [[ "$PLOT" == "python" ]]; then
         wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
         bash miniconda.sh -b -p $HOME/miniconda;
         export PATH="$HOME/miniconda/bin:$PATH";
