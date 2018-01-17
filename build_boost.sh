74508226832166987d3738454f289d80c68d0544
Dan <d.mapleson@gmail.com>
2018-01-17 23:01:09 +0000

    Speeding up boost build and fixing some links.

--- build_boost.sh
+++ build_boost.sh
@@ -1,11 +1,15 @@
 #!/bin/sh -e
 
+# Where the boost include and lib files get sent to
+INST_DIR=../boost_build
+
 # Ensure the boost submodule is present
 git submodule sync
 git submodule update --recursive --init deps/boost
 
 # Build boost
 cd deps/boost
-./bootstrap.sh --without-libraries=python,wave,math,graph_parallel,mpi,iostreams
-./b2 link=static
+./bootstrap.sh --prefix=${INST_DIR} --with-libraries=chrono,exception,program_options,timer,filesystem,system,stacktrace
+./b2 --prefix=${INST_DIR} headers
+./b2 --prefix=${INST_DIR} install
 cd ../..
