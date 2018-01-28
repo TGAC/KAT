8702fa972a63d155666e8e96b59b399d870c5cd5
Daniel Mapleson <daniel.mapleson@earlham.ac.uk>
2018-01-28 23:45:52 +0000

    Tidying up build boost script.
    Only running unit tests on Linux... Mac will currently fail in the largekeys test for jellyfish... probably no way around this for now.

--- build_boost.sh
+++ build_boost.sh
@@ -1,19 +1,11 @@
 #!/bin/sh -e
 
-# Where the boost include and lib files get sent to
-INST_DIR=build
-
-# Git method
-# Ensure the boost submodule is present
-#git submodule sync
-#git submodule update --recursive --init deps/boost
-
 # Using BCP
 #bcp algorithm build chrono exception filesystem locale program_options stacktrace system timer deps/boost
 
 # Build boost
 cd deps/boost
-./bootstrap.sh --prefix=${INST_DIR} --with-libraries=chrono,exception,program_options,timer,filesystem,system,stacktrace
+./bootstrap.sh --prefix=build --with-libraries=chrono,exception,program_options,timer,filesystem,system,stacktrace
 ./b2 headers
 ./b2 install
 cd ../..
