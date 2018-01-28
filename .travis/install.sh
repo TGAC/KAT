579a9c6f184d6475baa674157afea3423bdf400a
Daniel Mapleson <daniel.mapleson@earlham.ac.uk>
2018-01-28 16:34:09 +0000

    Tidy up travis scripts

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,31 +1,50 @@
 #!/bin/bash
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+    brew update
+    brew install autoconf automake libtool
 
     # install anaconda
-    wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh;
-    bash miniconda.sh -b -p $HOME/miniconda;
-    export PATH="$HOME/miniconda/bin:$PATH";
-    hash -r;
-    conda config --set always_yes yes --set changeps1 no;
-    conda update -q conda;
-    conda info -a
-    conda create -q -n test-environment python=3.5 anaconda;
+    wget https://repo.continuum.io/miniconda/Miniconda3-4.3.31-MacOSX-x86_64.sh -O miniconda.sh;
 
 else
+    sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+    sudo apt-get update -qq
+    
+    # Install Autotools
+    sudo apt-get install -qq autoconf automake libtool
+    
     # Install zlib
-    sudo apt-get install zlib1g zlib1g-dev
+    sudo apt-get install -qq zlib1g zlib1g-dev
+    
+    if [ "$COMPILER" == "GCC5" ]; then
+        sudo apt-get install -qq gcc-5.4 g++-5.4
+        sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-5.4 100
+        sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5.4 100
+        export CXX="g++-5.4"
+        export CC="gcc-5.4"
+    else
+        sudo apt-get install -qq gcc-4.9 g++-4.9
+        sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.9 100
+        sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.9 100
+        export CXX="g++-4.9"
+        export CC="gcc-4.9"
+    fi
+    gcc --version
+    g++ --version
+
 
     # Plotting installation
     if [[ "$PLOT" == "python" ]]; then
-        wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
-        bash miniconda.sh -b -p $HOME/miniconda;
-        export PATH="$HOME/miniconda/bin:$PATH";
-        hash -r;
-        conda config --set always_yes yes --set changeps1 no;
-        conda update -q conda;
-        conda info -a
-        conda create -q -n test-environment python=3.5 anaconda;
+        wget https://repo.continuum.io/miniconda/Miniconda3-4.3.31-Linux-x86_64.sh -O miniconda.sh;
     fi
 
 fi
+
+bash miniconda.sh -b -p $HOME/miniconda;
+export PATH="$HOME/miniconda/bin:$PATH";
+hash -r;
+conda config --set always_yes yes --set changeps1 no;
+conda update -q conda;
+conda info -a
+conda create -q -n test-environment python=3.6 anaconda;
