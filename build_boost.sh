b1d8b8c10b4afecea41676bb7fb109067b6fbed6
maplesond <d.mapleson@gmail.com>
2018-04-22 21:41:57 +0100

    Ignoring boost site wide configuration file when installing boost.

--- build_boost.sh
+++ build_boost.sh
@@ -6,6 +6,6 @@
 # Build boost
 cd deps/boost
 ./bootstrap.sh --prefix=build --with-libraries=chrono,exception,program_options,timer,filesystem,system,stacktrace
-./b2 headers
-./b2 install
+./b2 --ignore-site-config headers
+./b2 --ignore-site-config install
 cd ../..
